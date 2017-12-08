7d222167853d81988dddedd3cd31068e1fc0c278
Elisee A <ansene.e@sfeir.com>
2017-12-08 19:17:03 +0100

    setting

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello from OpenShift\n");
+}).listen(port);
+
+console.log("Server running on port " + port);
