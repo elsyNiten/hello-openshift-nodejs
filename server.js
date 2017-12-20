703dbdb00593ae3232ccf89e7374f525c56d97d0
Elisee A <ansene.e@sfeir.com>
2017-12-20 03:19:30 +0100

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 http.createServer((request, response) => {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello from OpenShift\n");
+  response.end("Hello from OpenShift\ndeployed launch by githook");
 }).listen(port);
 
 console.log("Server running on port " + port);
